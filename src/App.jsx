05e064b51b82952df4b4eb9acd64f2f1f66f110c
franklpjr <eng.civ.frank@gmail.com>
2023-05-20 13:33:35 -0400

    Set button back to instagram

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,19 @@ import Maranatalogo from './assets/Maranatalogo.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="./assets/logomaranata.png" target="_blank" rel="noreferrer">
+        <a href="https://www.instagram.com/maranata.clube/" target="_blank" rel="noreferrer">
           <img src={Maranatalogo} className="logo" alt="Maranata logo" />
         </a>
       </div>
       <h1 className='title'>CLUBE DE DESBRAVADORES MARANATA</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <form action="https://www.instagram.com/maranata.clube/">
+          <input type="submit" value="Voltar para o instagram" />
+        </form>
         <p>
           Página em desenvolvimento. Em breve uma nova experiência na palma de suas mãos!
         </p>
